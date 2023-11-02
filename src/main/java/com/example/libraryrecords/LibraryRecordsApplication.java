35f4cca4212a5a5217f444d29ce7d277e7c4070e
Serhii Ilnytskyi <SerhiiIlnytskyi@gmail.com>
2023-11-02 12:35:24 +0200

    TE-0: initialized project. Added maven dependencies

---
+++ src/main/java/com/example/libraryrecords/LibraryRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRecordsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LibraryRecordsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/libraryrecords/LibraryRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRecordsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
